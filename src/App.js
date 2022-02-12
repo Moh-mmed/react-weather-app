25cce73e3fced7f69d8af4000258faa0bb03041c
mohammedBnomr <moohaboy@gmail.com>
2022-02-12 22:21:50 +0300

    fixinig bug for state conflict

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom"
-import axios from "axios"
 import './App.css';
+import axios from "axios"
 import Body from "./Components/Main/Body";
 import Nav from "./Components/Main/NavBar";
 import SideBar from "./Components/SideBar";
 import TodaysTemperatures from "./Components/Main/Body/TodaysTemperatures";
 import NextFiveDays from "./Components/Main/Body/NextFiveDays";
 import ErrorPage from './Components/ErrorPage'
+
+
 function App() {
-  const [searchCity, setSearchCity] = useState("istanbul")
-  const [coords, setCoords] = useState({ lat: 41.01, lon: 28.91})
-  const [data, setData] = useState()
-  const findWeather = async () => {
-      const KEY = "99eb51721a50689c8175af2560a2b07c";
-      const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${KEY}`;
-      const response = await axios.get(URL, {
-        headers: { Accept: "application/json" },
-      });
-    setData(response.data);
-    console.log("Finding weather")
-  }
+  const [searchCity, setSearchCity] = useState(null)
+  const [coords, setCoords] = useState(null)
+  const [data, setData] = useState(null)
+  const KEY = "99eb51721a50689c8175af2560a2b07c";
+  
+  
   const handleSearchCity = (e) => {
     setSearchCity(e);
   }
 
-  const getLocation = () => {
+  //Find coordinates using location
+  const getLocation = async () => {
+    let coordinates;
     let opts = {
       enableHightAccuracy: true,
       timeout: 1000 * 10,
@@ -33,21 +31,88 @@ function App() {
     }
     let fail = (err) => console.log(err);
     let success = (position) => {
-      setCoords({
+      coordinates = {
         lat: Number(position.coords.latitude.toFixed(2)),
-        lon: Number(position.coords.longitude.toFixed(2))
-      })
+        lon: Number(position.coords.longitude.toFixed(2)),
+      }; 
+      setCoords(coordinates);
+      // localStorage.setItem("coordinates", JSON.stringify(coordinates));
     };
-    navigator.geolocation.getCurrentPosition(success, fail, opts);
+    await navigator.geolocation.getCurrentPosition(success, fail, opts);
   }
+  
 
   useEffect(() => {
-    getLocation();
-    //API call
-    findWeather()
-    console.log(data);
+ 
+  const findCoordinates = async () => {
+      let coordinatesURL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=5&appid=${KEY}`;
+      await axios
+        .get(coordinatesURL, {
+          headers: { Accept: "application/json" },
+        })
+        .then((response) => {
+          if (response.data.length > 0) {
+            setCoords({
+              lat: Number(response.data[0].lat.toFixed(2)),
+              lon: Number(response.data[0].lon.toFixed(2)),
+            });
+            setSearchCity(response.data[0].name);
+          } else {
+             console.log("You entered a wrong city");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+  //  let coordinates = JSON.parse(localStorage.getItem('coordinates')) || getLocation();
+   getLocation()
+  //  console.log(coords)
+    if (searchCity !== null) {
+     findCoordinates();
+     console.log("Search city is running")
+    }
+    
   }, [searchCity]);
-  
+
+  useEffect(() => {
+  console.log("4");
+    const findWeather = async () => {
+     const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${KEY}`;
+     await axios
+       .get(weatherURL, {
+         headers: { Accept: "application/json" },
+       })
+       .then((response) => {
+         setData(response.data);
+         console.log(response.data)
+       })
+       .catch((err) => {
+         setData(console.log(err));
+       });
+    };
+    const findCityName = async () => {
+      const reverseURL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${coords.lat}&lon=${coords.lon}&limit=5&appid=${KEY}`;
+       await axios
+         .get(reverseURL, {
+           headers: { Accept: "application/json" },
+         })
+         .then((response) => {
+           setSearchCity(response.data[0].name)
+           console.log(response.data);
+         })
+         .catch((err) => {
+          //  setData(console.log(err));
+         });
+    };
+    if (coords !== null) {
+      findWeather()
+      findCityName();
+    }
+ }, [coords]);
+
+
   return (
     <div className="App">
       <div className="Main">
---
+++ src/helpers/apiCalls.js
@@ -0,0 +1,15 @@
+// import axios from "axios";
+// export const findWeather = async (setData) => {
+//   let KEY = "99eb51721a50689c8175af2560a2b07c";
+//   let URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${KEY}`;
+//   axios
+//     .get(URL, {
+//       headers: { Accept: "application/json" },
+//     })
+//     .then((response) => {
+//       setData(response.data);
+//     })
+//     .catch((err) => {
+//       setData(console.log(err));
+//     });
+// };
